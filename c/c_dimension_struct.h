1f1daabae91ecbdff5bb8d848c10770177720524
botszhuang <22925483+botszhuang@users.noreply.github.com>
2025-12-04 15:14:05 +0800

    c_dimension.h

---
+++ c/c_dimension_struct.h
@@ -0,0 +1,12 @@
+#ifndef C_DIMENSION_STRUCT_H
+#define C_DIMENSION_STRUCT_H
+#include <stdlib.h>
+typedef struct {
+    double x, y, z ;
+}c_3D_struct;
+
+typedef struct {
+    double x, y ;
+}c_2D_struct;
+
+#endif
